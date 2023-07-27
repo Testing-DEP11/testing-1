65a20ba222b9953d9d74309b4961869c162f0f46
Lakith Rathnayake <lakithpathmila@gmail.com>
2023-07-28 00:02:58 +0530

    version 6.0

--- Java2.java
+++ Java2.java
@@ -1,6 +1,18 @@
-public class Java2 {
+// public class Java2 {
+//     public static void main(String[] args) {
+//         System.out.println("another java file is added");
+//         System.out.println("text file was added");
+//     }
+// }
+
+public class Java2{
+    public int add(int a, int b){
+        return a + b;
+    }
     public static void main(String[] args) {
-        System.out.println("another java file is added");
-        System.out.println("text file was added");
+        Java2 math = new Java2();
+        int result = math.add(3,5);
+        System.out.println(result);
     }
 }
+

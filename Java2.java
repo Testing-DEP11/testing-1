f53ae508868e543ba0c524938de1225012a04c71
Lakith Rathnayake <lakithpathmila@gmail.com>
2023-07-27 23:14:32 +0530

    version 5.0

--- Java2.java
+++ Java2.java
@@ -1,5 +1,6 @@
 public class Java2 {
     public static void main(String[] args) {
         System.out.println("another java file is added");
+        System.out.println("text file was added");
     }
 }
